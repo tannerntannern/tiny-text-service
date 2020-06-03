fcd1b8127d9262e34f76aa5cb0dc170ed04e1627
Tanner Nielsen <tanner.nielsen@nexteraanalytics.com>
2020-06-02 21:39:33 -0500

    Debugging

--- lib/handlers.ts
+++ lib/handlers.ts
@@ -1,12 +1,8 @@
 import { NowRequest, NowResponse } from '@now/node';
 import { text } from 'micro';
 import { parse } from 'querystring';
-import axios from 'axios';
 
 import convert, { charSets } from './convert';
-
-const slackApiBaseUrl = process.env.SLACK_API_BASE_URL as string;
-const slackBotUserOauthToken = process.env.SLACK_BOT_USER_OAUTH_TOKEN as string;
 
 export const handler = (type: keyof typeof charSets) =>
   (req: NowRequest, res: NowResponse) =>
@@ -19,31 +15,12 @@ export const slackHandler = (type: keyof typeof charSets) =>
     const payload = parse(await text(req)) as SlashCommandPayload;
     const convertedText = convert(payload.text as string, type);
 
+    console.log(payload.response_url.slice(0, 16));
+
     res.json({
-      response_type: 'ephemeral',
+      response_type: 'in_channel',
       text: convertedText,
     })
-
-    // const headers = {
-    //   'Content-Type': 'application/json',
-    //   'Authorization': `Bearer ${slackBotUserOauthToken}`
-    // };
-
-    // const body = {
-    //   channel: payload.channel_id,
-    //   text: convertedText,
-    // };
-
-    // const response = await axios.post<PostMessageResponse>(`${slackApiBaseUrl}/chat.postMessage`, body, { headers });
-    // if (!response.data.ok) {
-    //   console.log(JSON.stringify(response.data));
-    //   res.json({
-    //     response_type: 'ephemeral',
-    //     text: response.data.error,
-    //   });
-    // } else {
-    //   res.send('');
-    // }
   }
 
 // https://api.slack.com/interactivity/slash-commands#app_command_handling
@@ -52,9 +29,5 @@ type SlashCommandPayload = {
   text: string,
   user_name: string,
   channel_id: string,
+  response_url: string,
 };
-
-type PostMessageResponse = {
-  ok: boolean,
-  error?: string,
-} & { [x: string]: any };
