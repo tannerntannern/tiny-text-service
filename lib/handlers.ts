f3d740e2d197466e8666632f1afcd350da1aeed8
Tanner Nielsen <tanner.nielsen@nexteraanalytics.com>
2020-06-02 20:11:21 -0500

    Made slack endpoints post as user (required oauth token)

--- lib/handlers.ts
+++ lib/handlers.ts
@@ -1,8 +1,11 @@
 import { NowRequest, NowResponse } from '@now/node';
 import { text } from 'micro';
 import { parse } from 'querystring';
+import axios from 'axios';
 
 import convert, { charSets } from './convert';
+
+const slackBotUserOauthToken = process.env.SLACK_BOT_USER_OAUTH_TOKEN as string;
 
 export const handler = (type: keyof typeof charSets) =>
   (req: NowRequest, res: NowResponse) =>
@@ -11,11 +14,30 @@ export const handler = (type: keyof typeof charSets) =>
     );
 
 export const slackHandler = (type: keyof typeof charSets) =>
-  async (req: NowRequest, res: NowResponse) =>
-    res.json({
-      response_type: 'in_channel',
-      text: convert(
-        parse(await text(req)).text as string,
-        type,
-      ),
-    });
+  async (req: NowRequest, res: NowResponse) => {
+    const payload = parse(await text(req)) as SlackPayload;
+    const convertedText = convert(payload.text as string, type);
+
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${slackBotUserOauthToken}`
+    };
+
+    const body = {
+      channel: payload.channel_id,
+      text: convertedText,
+      as_user: true,
+    };
+
+    await axios.post('https://slack.com/api/chat.postMessage', body, { headers })
+
+    res.status(200).send('');
+  }
+
+// https://api.slack.com/interactivity/slash-commands#app_command_handling
+type SlackPayload = {
+  command: string,
+  text: string,
+  user_name: string,
+  channel_id: string,
+};
