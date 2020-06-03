87f3b32d0bc3c977f325cc73a142b34226e5a8d6
Tanner Nielsen <tanner.nielsen@nexteraanalytics.com>
2020-06-02 21:24:20 -0500

    Debugging

--- lib/handlers.ts
+++ lib/handlers.ts
@@ -19,26 +19,31 @@ export const slackHandler = (type: keyof typeof charSets) =>
     const payload = parse(await text(req)) as SlashCommandPayload;
     const convertedText = convert(payload.text as string, type);
 
-    const headers = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${slackBotUserOauthToken}`
-    };
-
-    const body = {
-      channel: payload.channel_id,
+    res.json({
+      response_type: 'ephemeral',
       text: convertedText,
-    };
+    })
 
-    const response = await axios.post<PostMessageResponse>(`${slackApiBaseUrl}/chat.postMessage`, body, { headers });
-    if (!response.data.ok) {
-      console.log(JSON.stringify(response.data));
-      res.json({
-        response_type: 'ephemeral',
-        text: response.data.error,
-      });
-    } else {
-      res.send('');
-    }
+    // const headers = {
+    //   'Content-Type': 'application/json',
+    //   'Authorization': `Bearer ${slackBotUserOauthToken}`
+    // };
+
+    // const body = {
+    //   channel: payload.channel_id,
+    //   text: convertedText,
+    // };
+
+    // const response = await axios.post<PostMessageResponse>(`${slackApiBaseUrl}/chat.postMessage`, body, { headers });
+    // if (!response.data.ok) {
+    //   console.log(JSON.stringify(response.data));
+    //   res.json({
+    //     response_type: 'ephemeral',
+    //     text: response.data.error,
+    //   });
+    // } else {
+    //   res.send('');
+    // }
   }
 
 // https://api.slack.com/interactivity/slash-commands#app_command_handling
